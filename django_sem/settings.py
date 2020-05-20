23d45988bdbc2535317b30abbe7da5429a1c0913
Rasim <rasim.2@bk.ru>
2020-05-21 02:51:54 +0300

    whitenoise fixing

--- django_sem/settings.py
+++ django_sem/settings.py
@@ -135,6 +135,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Directory for media files storing
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
